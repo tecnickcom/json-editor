280eeb509674b6c9792d0202031bf092d801d1d4
Andrea Di Toro <andrea@androidsx.com>
2017-02-22 11:00:17 +0100

    add/remove class utilities

--- src/utilities.js
+++ src/utilities.js
@@ -77,6 +77,31 @@ var $triggerc = function(el,event) {
 
   el.dispatchEvent(e);
 };
+var  $hasClass = function(el, className) {
+  if (el.classList){
+    return el.classList.contains(className);
+  }
+  else{
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  }
+};
+var $addClass = function(el, className) {
+  if (el.classList){
+    el.classList.add(className);
+  }
+  else if (!$hasClass(el, className)){
+    el.className += " " + className;
+  } 
+};
+var $removeClass = function(el, className) {
+  if (el.classList){
+    el.classList.remove(className);
+  }
+  else if ($hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    el.className=el.className.replace(reg, ' ');
+  }
+};
 var $deepCompare = function() {
   var i, l, leftChain, rightChain;
 
